ea652d73429e0327c73e02176f582acbb279ece4
okitta <okitta9388@gmail.com>
2024-02-11 23:03:50 +0300

    check

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0a62b06afb81b350"; }
+/******/ 		__webpack_require__.h = function() { return "263df8b03c1c5b93"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
